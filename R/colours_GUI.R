ee1dc7b6587a5f1e1f6ffbc88c7cff25b10c5dfd
*tnaake* <thomasnaake@gmx.de>
2014-05-02 18:56:15 +0100

    pRolocGUI v0.1.1, add saved searches

--- R/colours_GUI.R
+++ R/colours_GUI.R
@@ -1,2 +1,27 @@
-colours_GUI <- function(){
-  setStockcol(paste0(getStockcol(), 70))}
+#'@name colours_GUI
+#'
+#'@aliases colours_GUI
+#'
+#'@title Colours for pRolocGUI
+#'
+#'@description Sets the default colours as used in the package \code{pRoloc}.
+#'
+#'@usage colours_GUI(transparency=70)
+#'
+#'@export colours_GUI
+#'
+#'@param transparency Sets the transparency of plotted elements. 
+#'Between 0 and 100 and of type \code{numeric}. Default is 
+#'70 as used in \code{\link{pRoloc::pRoloc-Tutorial}}. 
+#'
+#'@author Thomas Naake <thomasnaake@@gmx.de>
+#'
+#'@examples \dontrun{colours_GUI(transparency=70)}
+#'
+#'@seealso \code{\link[pRoloc]{setStockcol}}
+#'
+colours_GUI <- function(transparency=70){
+  if (is.numeric(transparency) &&
+      transparency >= 0 && transparency <= 100){
+  if (nchar(getStockcol()[1])==7){
+  setStockcol(paste0(getStockcol(), transparency))}}}
