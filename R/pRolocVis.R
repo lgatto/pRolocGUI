5bc7342090b1650842ff7de3c6b3c70e60760e8a
*tnaake* <thomasnaake@gmx.de>
2014-05-29 15:37:59 +0100

    remove reactivePoll and rewrite search tab

--- R/pRolocVis.R
+++ R/pRolocVis.R
@@ -26,15 +26,17 @@ pRolocVis <- function(object = NULL) {
     ## is there a pRolocGUI_SearchResults? is so load it.
     ## possibly FeaturesOfInterest -> FoICollection
     ## else empty FoICollection
-    sr <- FoICollection()
-    
-    savesr <- function(sr) {
-      if (length(sr) > 0)
-        assign(sr, "pRolocGUI_SearchResults", .GobalEnv)
-      return(TRUE)
+    if (exists("pRolocGUI_SearchResults", .GlobalEnv)) {
+        sr <- get("pRolocGUI_SearchResults", .GlobalEnv)
+        if (inherits(sr, "FoICollection"))
+            sr <- foi(sr) 
+        else  ## "FeaturesOfInterest"
+            sr <- list(sr)
+    } else {
+        sr <- list(FeaturesOfInterest(description="empty", fnames = featureNames(andy2011)[0],
+                     object=andy2011))
     }
     
-    on.exit(savesr(sr))
     
     app <- list(  
         ui = 
@@ -227,9 +229,9 @@ pRolocVis <- function(object = NULL) {
                     isolate({
                         input$chooseIdenSearch
                         if (length(.protPCA$mult) > 2 && 
-                                !is.null(select$PCA) && 
-                                    "mousePCA" %in% select$PCA && 
-                                        !("mousePCA" %in% input$chooseIdenSearch))
+                            !is.null(select$PCA) && 
+                                "mousePCA" %in% select$PCA && 
+                                    !("mousePCA" %in% input$chooseIdenSearch))
                             select$PCA <- NULL
                     })
                 }
@@ -248,9 +250,9 @@ pRolocVis <- function(object = NULL) {
                     isolate({
                         input$chooseIdenSearch
                         if (length(.protPlotDist$mult) > 2 && 
-                                !is.null(select$plotDist) &&
-                                    "mousePlotDist" %in% select$plotDist && 
-                                        !("mousePlotDist" %in% input$chooseIdenSearch))
+                            !is.null(select$plotDist) &&
+                                "mousePlotDist" %in% select$plotDist && 
+                                    !("mousePlotDist" %in% input$chooseIdenSearch))
                             select$plotDist <- NULL
                     })
                 }
@@ -601,7 +603,8 @@ pRolocVis <- function(object = NULL) {
                         if (length(which(as.vector(table(.protPCA$mult)) > 1))) 
                             .protPCA$mult <- 
                                 .protPCA$mult[
-                                              -which(.protPCA$mult == names(which(table(.protPCA$mult) > 1)))
+                                    -which(.protPCA$mult 
+                                        == names(which(table(.protPCA$mult) > 1)))
                                               ]
                         
                     })   
@@ -683,7 +686,7 @@ pRolocVis <- function(object = NULL) {
                         if (length(which((as.vector(table(.protPlotDist$mult)) > 1))))
                             .protPlotDist$mult <- 
                                 .protPlotDist$mult[-which(.protPlotDist$mult
-                                                          == names(which(table(.protPlotDist$mult) > 1)))]
+                                    == names(which(table(.protPlotDist$mult) > 1)))]
                     })
                 }
             }) 
@@ -848,156 +851,105 @@ pRolocVis <- function(object = NULL) {
             
             
             ## TAB: SEARCH ##
-            ## get object pRolocGUI_SearchResults from
-            ## the global environment and poll for changes
-            .pR_SR <- reactivePoll(
-              intervalMillis = 1000,
-              session = NULL,
-              checkFunc = .digestFOI,
-              valueFunc = .readSR
-            )
+            observe({
+                if (length(.pR_SR$foi) > 0)
+                    on.exit(assign("pRolocGUI_SearchResults", 
+                            FoICollection(.pR_SR$foi), .GlobalEnv)
+                    )
+            })
             
-            ## Get the tag names of the list with the saved search results
-            .tagsList <- reactivePoll(
-              intervalMillis = 1000,
-              session = NULL,
-              checkFunc = .digestFOI,
-              valueFunc = .descrFOI
-            )
+            .pR_SR <- reactiveValues(foi = sr)
+            
+            observe({
+                newFOI <- isolate(.newfoi$ind)
+              
+                if (input$saveLists2SR > 0 
+                    && !is.null(input$saveLists2SR) 
+                        && length(.searchInd()) > 0) {
+                
+                    isolate(
+                        if(!(input$savedSearchText %in% description(FoICollection(.pR_SR$foi)))) {
+                            .pR_SR$foi <- c(.pR_SR$foi, newFOI)
+                    })
+                    isolate(
+                        if(length(.pR_SR$foi[[1]]) == 0) {
+                            .pR_SR$foi <- c(.pR_SR$foi[-1])
+                        })
+                }            
+            })
+            
+            ## text field to assign name to search results
+            ## display information about selected FoI
+            output$infoSavedSearch <- renderText({
+                if (!is.null(.pR_SR$foi)) {
+                    showFOI <- .showFOI(.pR_SR$foi, .dI(), .whichN())
+                    paste0(showFOI, sep = "\n", collapse = "")
+                }
+                else
+                    return("pRolocGUI_SearchResults not found in workspace")
+            })
+            
+            .tagsList <- reactive({description(FoICollection(.pR_SR$foi))})
             
             .whichN <- reactive({
-              which(
-                input$tagSelectList ==
-                  description(.pR_SR())
-              )[1]
+                which(
+                    input$tagSelectList ==
+                    description(FoICollection(.pR_SR$foi))
+                )[1]
             })
             
             .whichNamesFOI <- reactive({
-              if (inherits(.pR_SR(), "FeaturesOfInterest"))
-                which(match(rownames(.dI()), .fnamesFOI(.pR_SR())) != "NA")
-              else
                 which(
-                  match(
-                    rownames(.dI()), .fnamesFOI(.pR_SR())[[.whichN()]]
-                  ) != "NA"
+                    match(
+                        rownames(.dI()), 
+                        .fnamesFOI(FoICollection(.pR_SR$foi))[[.whichN()]]
+                    ) != "NA"
                 )
             })
             
             ## select Input for the tag names of the list
             output$tagsListSearchResultUI <- renderUI(
-              if (exists("pRolocGUI_SearchResults", .GlobalEnv) |
-                    !is.null(.pR_SR()))
-                selectInput("tagSelectList",
-                            "Select search result",
-                            choices = .tagsList()
-                )
-            )
+              selectInput("tagSelectList",
+                          "Select search result",
+                          choices = .tagsList()
+              )
+            )     
             
-            ## display information about selected FoI
-            output$infoSavedSearch <- renderText({
-              if (exists("pRolocGUI_SearchResults", .GlobalEnv)|
-                    !is.null(.pR_SR())) {
-                showFOI <- .showFOI(.pR_SR(), .dI(), .whichN())
-                paste0(showFOI, sep = "\n", collapse = "")
-              }
-              else
-                return("pRolocGUI_SearchResults not found in workspace")
-            })
-            
-            ## text field to assign name to search results
             output$savedSearchTextUI <- renderUI(
-              if(exists("pRolocGUI_SearchResults", .GlobalEnv) ||
-                   !is.null(.pR_SR()))
-                textInput("savedSearchText",
-                          "Description",
-                          value="new search result")
+              textInput("savedSearchText",
+                        "Description",
+                        value="new search result")
             )
             
             ## action button to save new FoIs
             output$saveLists2SRUI <- renderUI({
-              if ((exists("pRolocGUI_SearchResults", .GlobalEnv) ||
-                     !is.null(.pR_SR())) && !is.null(input$savedSearchText)) {
-                if(!(input$savedSearchText %in% description(.pR_SR())))
-                  actionButton("saveLists2SR",
-                               "Create new features of interest"
-                  )
-                else
-                  return("name exists already, choose another name")
-              }
-            })
-            
-            ## Action Button when pRolocGUI_SearchResults does not exist
-            ## in .GlobalEnv --> initialize
-            output$initSaveUI <- renderUI({
-              if (!exists("pRolocGUI_SearchResults", .GlobalEnv)
-                  && is.null(.tagsList()))
-                actionButton("initSavedSearch",
-                             "Initialize saved searches"
-                )
-            })
-            
-            ## create new FoICollection to initialize saved searches and
-            ## assign it to the name pRolocGUI_SearchResult in .GlobalEnv
-            observe({
-              if (!is.null(input$initSavedSearch) && input$initSavedSearch > 0) {
-                initFoi <- FeaturesOfInterest(
-                  description = "empty",
-                  fnames = featureNames(tan2009r1)[0]
-                )
-                initColl <- FoICollection()
-                initColl <- addFeaturesOfInterest(initFoi, initColl)
-                assign("pRolocGUI_SearchResults",
-                       initColl, envir = .GlobalEnv)
-              }
-            })
-            
-            ## new features of Interest as a reactive expression
-            .newfoi <- reactive({
-              input$saveLists2SR
-              isolate({
-                FeaturesOfInterest(
-                  description = input$savedSearchText,
-                  fnames = featureNames(.dI())[.searchInd()],
-                  object = .dI())
-              })
-            })
-            
-            ## overwrite pRolocGUI_SearchResults with new features
-            ## of interest by assigning it in .GlobalEnv, treat
-            ## accordingly if .pR_SR() is FeaturesOfInterest or
-            ## collection of Features Of Interest
-            observe({
-              if (!is.null(input$saveLists2SR) && input$saveLists2SR > 0
-                  && !is.null(.searchInd()) && length(input$savedSearchText)) {
-                if (inherits(.pR_SR(), "FoICollection") &&
-                      length(.pR_SR()) == 1 && 
-                      !length(.fnamesFOI(.pR_SR(), flist = FALSE)))
-                  oldSR <- FoICollection()
-                else
-                  oldSR <- .pR_SR()
-                
-                newFOI <- .newfoi()
-                
-                if (inherits(.pR_SR(), "FeaturesOfInterest")){
-                  newColl <- FoICollection() ## create new collection
-                  ## add old FoI to collection
-                  newColl <- addFeaturesOfInterest(oldSR, newColl)
-                  newColl <- isolate({
-                    input$saveLists2SR
-                    addFeaturesOfInterest(newFOI, newColl)
-                  })
-                  assign("pRolocGUI_SearchResults", newColl, envir = .GlobalEnv)
+                if (length(input$savedSearchText) != 0){
+                    if(!(input$savedSearchText %in% 
+                           description(FoICollection(.pR_SR$foi))))
+                        actionButton("saveLists2SR",
+                            "Create new features of interest")
+                    else 
+                        return("name already exists, choose another name")
                 }
-                else {
-                  newColl <- isolate({
-                    input$saveLists2SR
-                    addFeaturesOfInterest(newFOI, oldSR)
-                  })
-                  assign("pRolocGUI_SearchResults", newColl, envir = .GlobalEnv)
-                }
-              } ## end if
-            })## end observe
+            })
             
+            .newfoi <- reactiveValues(ind = NULL)
+            
+            observe({
+                input$saveLists2SR
+                sI <- isolate(.searchInd())
+                searchText <- isolate(input$savedSearchText)
+                dataInput <- isolate(.dI())
+                if (!is.null(searchText) 
+                    && !is.null(dataInput) 
+                        && !is.null(sI))
+                    isolate({
+                        .newfoi$ind <- FeaturesOfInterest(
+                            description = searchText,
+                            fnames = featureNames(dataInput)[sI],
+                            object = dataInput)
+                    })
+            })            
             ### END: SEARCH ###
         
         } ## end server function        
