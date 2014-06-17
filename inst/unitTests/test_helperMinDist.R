d2231a803f7f69efea3a3b1e7c6c6e68a9d38cfc
*tnaake* <thomasnaake@gmx.de>
2014-06-17 20:15:04 +0100

    update unit tests

--- inst/unitTests/test_helperMinDist.R
+++ inst/unitTests/test_helperMinDist.R
@@ -5,14 +5,15 @@ valuesPCA <- plot2D(andy2011, fcol=NULL, plot = FALSE)
 
 test_.minDistPCA <- function() {
     checkEqualsNumeric(
-        pRolocGUI:::.minDistPCA(-4.05, 5.65, valuesPCA[, 1], valuesPCA[, 2]), 
-        922
-    )
+        pRolocGUI:::.minDistPCA(-4.047683, 5.663844, 
+                valuesPCA[, 1], valuesPCA[, 2]), 922)
     checkEquals(
-        names(pRolocGUI:::.minDistPCA(-4.05, 5.65, valuesPCA[, 1], valuesPCA[, 2])), 
-        "PARP1_HUMAN"
-    )
-}
+        pRolocGUI:::.minDistPCA(-4.047683, 5.663844, 
+                valuesPCA[, 1], valuesPCA[, 2], TRUE), "PARP1_HUMAN")
+    checkTrue(
+        is.null(pRolocGUI:::.minDistPCA(NULL, NULL, 
+                valuesPCA[, 1], valuesPCA[, 2])))
+} 
 ## END unit test .minDistPCA ## 
 
 
@@ -20,13 +21,20 @@ test_.minDistPCA <- function() {
 ## START unit test .minDistPlotDist ## 
 test_.minDistPlotDist <- function() {
     checkEqualsNumeric(
-        pRolocGUI:::.minDistPlotDist(andy2011, "all", "all", 3, 0.48), 
-        47
-    )
+        pRolocGUI:::.minDistPlotDist(list(andy2011, tan2009r1), 
+            "all", "all", 3, 0.4957, "object1", FALSE), 47)
+    checkEquals(
+        pRolocGUI:::.minDistPlotDist(list(andy2011, tan2009r1), 
+            "all", "all", 3, 0.4957, "object1", TRUE), "PGAP1_HUMAN")
     checkEqualsNumeric(
-        pRolocGUI:::.minDistPlotDist(andy2011, "markers", "ER", 1, 0.27),
-        29
-    )
+        pRolocGUI:::.minDistPlotDist(list(andy2011, tan2009r1), 
+            "markers", "ER", 1, 0.27, "object1", FALSE), 29)
+    checkEqualsNumeric(
+        pRolocGUI:::.minDistPlotDist(list(andy2011, tan2009r1), 
+            "all", "all", 4, 0.4435, "object2", FALSE), 383)
+    checkEqualsNumeric(
+        pRolocGUI:::.minDistPlotDist(list(andy2011, tan2009r1),
+            "all", "all", 1, 0, "object2", TRUE), "FBgn0004584")
 }
 ## END unit test .minDistPlotDist ## 
 
