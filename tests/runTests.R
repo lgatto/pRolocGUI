3dd46f97cffb75168e889ba62639b2f5338ceea8
*tnaake* <thomasnaake@gmx.de>
2014-06-25 15:36:10 +0100

    update unit tests

--- tests/runTests.R
+++ tests/runTests.R
@@ -2,5 +2,6 @@ library(pRoloc)
 library(pRolocdata)
 data(andy2011, package = "pRolocdata")
 data(tan2009r1, package = "pRolocdata")
+data(tan2009r2, package = "pRolocdata")
 
 BiocGenerics:::testPackage("pRolocGUI")
