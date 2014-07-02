7cfce8a186ee086e70ebe30d2a843137c399006f
Laurent Gatto <l.gatto@dnavision.be>
2014-07-02 00:18:25 +0000

    cosmetic changes
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRolocGUI@92085 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/pRolocGUI-package.R
+++ R/pRolocGUI-package.R
@@ -1,15 +1,10 @@
 #'@name pRolocGUI-package
-#'
 #'@aliases pRolocGUI-package
-#'
 #'@docType package
-#'
 #'@title An interactive visualisation tool for organelle (spatial) proteomics data
-#'
 #'@description The package pRolocGUI comprises functions to interactively 
 #'visualise organelle (spatial) proteomics data on the basis of pRoloc,
 #'pRolocdata and shiny. 
-#'
 #'@details \tabular{ll}{
 #'Package: \tab pRolocGUI \cr
 #'Type: \tab Package \cr
@@ -17,20 +12,14 @@
 #'Date: \tab 2014-04-28 \cr
 #'License: \tab GPL-2 \cr
 #'}
-#'
 #'@author {Thomas Naake and Laurent Gatto \cr
 #'Maintainer: Thomas Naake <thomasnaake@@gmx.de>
 #'}
-#'
 #'@seealso \code{\link[pRoloc]{pRoloc-tutorial}}
 #'\code{\link[pRolocdata]{pRolocdata}}
 #'\code{\link[MSnbase]{FeaturesOfInterest-class}}
-#'
 #'@examples \dontrun{pRolocGUI()}
-#'
 #'@import pRoloc pRolocdata shiny
-#'
 #'@importFrom digest digest
-#'
 #'@importFrom tools file_ext
 NULL
