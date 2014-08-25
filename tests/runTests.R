4518518b4a079d239a2ba6b2e2edeea22d0a3da0
l.gatto <l.gatto@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-08-25 21:30:00 +0000

    manual sync with git
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/pRolocGUI@93592 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/runTests.R
@@ -0,0 +1,7 @@
+library(pRoloc)
+library(pRolocdata)
+data(andy2011, package = "pRolocdata")
+data(tan2009r1, package = "pRolocdata")
+data(tan2009r2, package = "pRolocdata")
+
+BiocGenerics:::testPackage("pRolocGUI")
