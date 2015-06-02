8c93fa0a027d600c9b498fb21a13a1eae70304af
Laurent <lg390@cam.ac.uk>
2015-06-02 01:59:32 +0100

    table section with brushing or usr

--- R/pRolocVis2.R
+++ R/pRolocVis2.R
@@ -1,4 +1,3 @@
-
 ## References
 ## https://gallery.shinyapps.io/095-plot-interaction-advanced/
 ## https://gallery.shinyapps.io/105-plot-interaction-zoom/
@@ -19,13 +18,14 @@
 ##' data(dunkley2006)
 ##' ## adds matrix markers
 ##' dunkley2006 <- mrkVecToMat(dunkley2006)
+##' dunkley2006 <- dunkley2006[, order(dunkley2006$fraction)]
 ##' pRolocVis2(dunkley2006)
 pRolocVis2 <- function(object, fcol = "Markers",
                        fig.height = "600px",
                        fig.width = "600px",
                        ...) {
     if (!inherits(object, "MSnSet"))
-        stxoop("The input must be of class MSnSet")
+        stop("The input must be of class MSnSet")
     if (is.null(fData(object)[, fcol]))
         stop("fcol missing in fData")
     pmarkers <- fData(object)[, fcol]
@@ -44,7 +44,7 @@ pRolocVis2 <- function(object, fcol = "Markers",
         cols <- rep(cols, n + 1)
     }
     ## Build shiny app
-    ui <- shinyUI(fluidPage(
+    ui <- fluidPage(
         sidebarLayout(
             sidebarPanel(
                 selectizeInput("markers", "Markers",
@@ -53,7 +53,7 @@ pRolocVis2 <- function(object, fcol = "Markers",
                                selected = colnames(pmarkers)),
                 sliderInput("trans", "Transparancy",
                             min = 0,  max = 1, value = 0.5),
-                plotOutput("legend"),
+                plotOutput("legend"),                
                 width = 2),
             mainPanel(
                 tabsetPanel(type = "tabs",
@@ -85,90 +85,97 @@ pRolocVis2 <- function(object, fcol = "Markers",
                                               DT::dataTableOutput("brushDataTable"))))
                             )
                 )
-            )))
+            ))
     
     server <-
-        shinyServer(function(input, output, session) {
-                        ranges <- reactiveValues(x = NULL, y = NULL)                        
-                        ## Get coords for proteins according to GO term specified in input
-                        pcaMrkSel <- reactive({
-                            lapply(input$markers,
-                                   function(z) pcas[which(pmarkers[, z] == 1), ])
-                        })
-                        profMrkSel <- reactive({
-                            lapply(input$markers,
-                                   function(z) profs[which(pmarkers[, z] == 1), ])
-                        })
+        function(input, output, session) {
+            ranges <- reactiveValues(x = NULL, y = NULL)
+            ## Get coords for proteins according to GO term specified in input
+            pcaMrkSel <- reactive({
+                lapply(input$markers,
+                       function(z) pcas[which(pmarkers[, z] == 1), ])
+            })
+            profMrkSel <- reactive({
+                lapply(input$markers,
+                       function(z) profs[which(pmarkers[, z] == 1), ])
+            })
 
-                        ## Update colour transparacy according to slider input
-                        myCols <- reactive({
-                            scales::alpha(cols, input$trans)[
-                                                             sapply(input$markers, function(z) 
-                                                                 which(colnames(pmarkers) == z))]
-                        })
-                        ## PCA plot
-                        output$pca <- renderPlot({
-                            par(mar = c(5.1, 4.1, 0, 1))
-                            plot(pcas,
-                                 col = getUnknowncol(),
-                                 pch = 21, cex = 1,
-                                 xlim = ranges$x,
-                                 ylim = ranges$y)
-                            for (i in 1:length(input$markers)) 
-                                points(pcaMrkSel()[[i]], pch = 16, cex = 1.4, col = myCols()[i])
-                            s <- input$brushDataTable_rows_selected
-                            if (length(s))
-                                points(pcas[s, , drop = FALSE], pch = 19, cex = 2)
-                        })
-                        ## Protein profile
-                        output$profile <- renderPlot({
-                            par(mar = c(5.1, 4.1, 0, 1))
-                            matplot(t(profs),
-                                    col = getUnknowncol(),
-                                    lty = 1,
-                                    type = "l")
-                            for (i in 1:length(input$markers)) 
-                                matlines(t(profMrkSel()[[i]]),
-                                         col = myCols()[i],
-                                         lty = 1,
-                                         lwd = 1.5)
-                        })                        
-                        ## Freature data table
-                        output$brushDataTable <- DT::renderDataTable({
-                            if (is.null(input$pcaBrush)) {
-                                i <- j <- TRUE
-                            } else {
-                                i <- pcas[, 1] >= input$pcaBrush$xmin & pcas[, 1] <= input$pcaBrush$xmax
-                                j <- pcas[, 2] >= input$pcaBrush$ymin & pcas[, 2] <= input$pcaBrush$ymax
-                            }
-                            DT::datatable(fData(object)[i & j, ],
-                                          rownames = TRUE,
-                                          options = list(searchHighlight = TRUE))
-                        })
-                        
-                        ## When a double-click happens, check if there's a brush on the plot.
-                        ## If so, zoom to the brush bounds; if not, reset the zoom.
-                        observeEvent(input$pcaDblclick, {
-                                         brush <- input$pcaBrush
-                                         if (!is.null(brush)) {
-                                             ranges$x <- c(brush$xmin, brush$xmax)
-                                             ranges$y <- c(brush$ymin, brush$ymax)
-                                         } else {
-                                             ranges$x <- NULL
-                                             ranges$y <- NULL
-                                         }
-                                     })
-                        
-                        ## Output legend
-                        output$legend <- renderPlot({
-                            legend("center",
-                                   input$markers,
-                                   col = myCols(),
-                                   inset = 1/10,
-                                   ncol = 1, bty = "n",
-                                   pch = 16, cex = .75)
-                        })
-                    })
+            ## Update colour transparacy according to slider input
+            myCols <- reactive({
+                scales::alpha(cols, input$trans)[
+                                                 sapply(input$markers, function(z) 
+                                                     which(colnames(pmarkers) == z))]
+            })
+            ## PCA plot
+            output$pca <- renderPlot({
+                par(mar = c(5.1, 4.1, 1, 1))
+                plot(pcas,
+                     col = getUnknowncol(),
+                     pch = 21, cex = 1,
+                     xlim = ranges$x,
+                     ylim = ranges$y)
+                usr <<- par("usr")
+                for (i in 1:length(input$markers)) 
+                    points(pcaMrkSel()[[i]], pch = 16, cex = 1.4, col = myCols()[i])
+                s <- input$brushDataTable_rows_selected
+                if (length(s))
+                    points(pcas[s, , drop = FALSE], pch = 19, cex = 2)
+            })
+            ## Protein profile
+            output$profile <- renderPlot({
+                par(mar = c(5.1, 4.1, 1, 1))
+                matplot(t(profs),
+                        col = getUnknowncol(),
+                        lty = 1,
+                        type = "l")
+                for (i in 1:length(input$markers)) 
+                    matlines(t(profMrkSel()[[i]]),
+                             col = myCols()[i],
+                             lty = 1,
+                             lwd = 1.5)
+                s <- input$brushDataTable_rows_selected
+                if (length(s))
+                    matlines(t(profs[s, ]),
+                             col = "black",
+                             lty = 1,
+                             lwd = 2)
+            })                        
+            ## Freature data table
+            output$brushDataTable <- DT::renderDataTable({
+                if (is.null(input$pcaBrush)) {
+                    i <- try(pcas[, 1] >= usr[1] & pcas[, 1] <= usr[2])
+                    j <- try(pcas[, 2] >= usr[3] & pcas[, 2] <= usr[4])
+                } else {
+                    i <- pcas[, 1] >= input$pcaBrush$xmin & pcas[, 1] <= input$pcaBrush$xmax
+                    j <- pcas[, 2] >= input$pcaBrush$ymin & pcas[, 2] <= input$pcaBrush$ymax
+                }
+                DT::datatable(fData(object)[i & j, ],
+                              rownames = TRUE,
+                              options = list(searchHighlight = TRUE),
+                              filter = 'top')
+            })                        
+            ## When a double-click happens, check if there's a brush on the plot.
+            ## If so, zoom to the brush bounds; if not, reset the zoom.
+            observeEvent(input$pcaDblclick, {
+                             brush <- input$pcaBrush
+                             if (!is.null(brush)) {
+                                 ranges$x <- c(brush$xmin, brush$xmax)
+                                 ranges$y <- c(brush$ymin, brush$ymax)
+                             } else {
+                                 ranges$x <- NULL
+                                 ranges$y <- NULL
+                             }                                         
+                         })
+            ## Output legend
+            output$legend <- renderPlot({
+                legend("center",
+                       input$markers,
+                       col = myCols(),
+                       inset = 1/10,
+                       ncol = 1, bty = "n",
+                       pch = 16, cex = .75)
+            })
+        }
     app <- list(ui = ui, server = server)
     runApp(app)
 }
